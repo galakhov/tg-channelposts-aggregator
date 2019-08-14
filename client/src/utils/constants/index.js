aae24cfd4e203f009a602bd248879c770d82014c
D. Galakhov <dmitri.galakhov@gmail.com>
2019-08-14 21:29:54 +0200

    Create optimized production build

--- client/src/utils/constants/index.js
+++ client/src/utils/constants/index.js
@@ -1,5 +1,4 @@
 // For successful deployment, add your domain in here (also needed for CI/CD)
-// production
 const port = process.env.PORT || 8080
 export const API_HOST =
   process.env.NODE_ENV === 'production'
