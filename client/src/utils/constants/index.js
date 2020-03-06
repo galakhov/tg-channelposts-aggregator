a2f800ee96346fc7976e5663b0b50c36a0ef27d5
D. Galakhov <dmitri.galakhov@gmail.com>
2020-03-06 23:16:49 +0100

    Fix: REACT_APP_ENVs

--- client/src/utils/constants/index.js
+++ client/src/utils/constants/index.js
@@ -1,14 +1,14 @@
-require('dotenv').config()
+// require('dotenv').config()
 // For successful deployment, add your domain in here (also needed for CI/CD)
 const port = process.env.PORT || 8080
 
 // console.log('process.env.REACT_APP_SERVER_IP:', process.env.REACT_APP_SERVER_IP)
-console.log('process.env.npm_config_host: ' + process.env.npm_config_host)
+console.log('process.env.npm_config_host: ' + process.env.REACT_APP_HOST)
 
 export const API_HOST =
   process.env.NODE_ENV === 'production'
-    ? process.env.npm_config_host
-      ? `http://${process.env.npm_config_host}:${port}`
+    ? process.env.REACT_APP_HOST
+      ? `http://${process.env.REACT_APP_HOST}:${port}`
       : '.'
     : `http://localhost:${port}`
 
