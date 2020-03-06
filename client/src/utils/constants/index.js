dd29ae24b2e66e9605c3d96ea2e25239883feba4
D. Galakhov <dmitri.galakhov@gmail.com>
2020-03-06 20:55:19 +0100

    Deployment settings

--- client/src/utils/constants/index.js
+++ client/src/utils/constants/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 // For successful deployment, add your domain in here (also needed for CI/CD)
 const port = process.env.PORT || 8080
+
+console.log('process.env.SERVER_IP', process.env.SERVER_IP)
+
 export const API_HOST =
   process.env.NODE_ENV === 'production'
     ? process.env.SERVER_IP
       ? `http://${process.env.SERVER_IP}:${port}`
       : '.'
     : `http://localhost:${port}`
-// first one was: `https://aggregator.cleverapps.io`
