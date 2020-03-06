2ead03c07636bda104108c11554b6deda867e3ed
D. Galakhov <dmitri.galakhov@gmail.com>
2020-03-06 19:04:33 +0100

    Add edge cases

--- client/src/utils/constants/index.js
+++ client/src/utils/constants/index.js
@@ -1,8 +1,10 @@
-// require('dotenv').config()
+require('dotenv').config()
 // For successful deployment, add your domain in here (also needed for CI/CD)
 const port = process.env.PORT || 8080
 export const API_HOST =
   process.env.NODE_ENV === 'production'
-    ? `http://${process.env.SERVER_IP}:${port}`
+    ? process.env.SERVER_IP
+      ? `http://${process.env.SERVER_IP}:${port}`
+      : '.'
     : `http://localhost:${port}`
 // first one was: `https://aggregator.cleverapps.io`
