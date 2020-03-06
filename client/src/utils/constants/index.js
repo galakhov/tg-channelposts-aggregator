ceb455bee733e2b294d36f02205d8d0f32143a71
D. Galakhov <dmitri.galakhov@gmail.com>
2020-03-06 22:27:28 +0100

    Add npm_config_env

--- client/src/utils/constants/index.js
+++ client/src/utils/constants/index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 // For successful deployment, add your domain in here (also needed for CI/CD)
 const port = process.env.PORT || 8080
 
-console.log('process.env.SERVER_IP', process.env.REACT_APP_SERVER_IP)
+// console.log('process.env.REACT_APP_SERVER_IP:', process.env.REACT_APP_SERVER_IP)
+console.log('process.env.npm_config_host: ' + process.env.npm_config_host)
 
 export const API_HOST =
   process.env.NODE_ENV === 'production'
