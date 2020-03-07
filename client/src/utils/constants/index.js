ce6814834d0b85925cb2ab668636ab379babd555
D. Galakhov <dmitri.galakhov@gmail.com>
2020-03-07 18:56:05 +0100

    Fix: change the frontend's exposed port

--- client/src/utils/constants/index.js
+++ client/src/utils/constants/index.js
@@ -1,6 +1,6 @@
 // require('dotenv').config()
 // For successful deployment, add your domain in here (also needed for CI/CD)
-const port = process.env.PORT || 8080
+const port = process.env.REACT_APP_PORT || 80
 
 console.log(
   'Passed ENV variable: REACT_APP_HOST_IP: ' + process.env.REACT_APP_HOST_IP
