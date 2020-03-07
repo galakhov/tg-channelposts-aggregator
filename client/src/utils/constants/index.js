5802643b0c036985ab4f0f86bca58193b0e967d1
D. Galakhov <dmitri.galakhov@gmail.com>
2020-03-07 10:07:43 +0100

    Docker Hub: naive trial of passing the ENV variables

--- client/src/utils/constants/index.js
+++ client/src/utils/constants/index.js
@@ -2,12 +2,14 @@
 // For successful deployment, add your domain in here (also needed for CI/CD)
 const port = process.env.PORT || 8080
 
-// console.log('process.env.REACT_APP_SERVER_IP:', process.env.REACT_APP_SERVER_IP)
-console.log('process.env.npm_config_host: ' + process.env.REACT_APP_HOST)
+console.log('process.env.REACT_APP_HOST_IP:', process.env.REACT_APP_HOST_IP)
+console.log('process.env.REACT_APP_HOST: ' + process.env.REACT_APP_HOST)
 
 export const API_HOST =
   process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_HOST
+    ? process.env.REACT_APP_HOST_IP
+      ? `http://${process.env.REACT_APP_HOST_IP}:${port}`
+      : process.env.REACT_APP_HOST
       ? `http://${process.env.REACT_APP_HOST}:${port}`
       : '.'
     : `http://localhost:${port}`
