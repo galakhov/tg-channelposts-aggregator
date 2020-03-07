f091756f76ef6dcbd94ec75077612b3d8565b3ae
D. Galakhov <dmitri.galakhov@gmail.com>
2020-03-07 12:23:23 +0100

    Docker Hub: polish the settings of the build hook

--- client/src/utils/constants/index.js
+++ client/src/utils/constants/index.js
@@ -2,17 +2,13 @@
 // For successful deployment, add your domain in here (also needed for CI/CD)
 const port = process.env.PORT || 8080
 
-console.log('process.env.REACT_APP_HOST_IP: ' + process.env.REACT_APP_HOST_IP)
-console.log('process.env.REACT_APP_HOST: ' + process.env.REACT_APP_HOST)
+console.log(
+  'Passed ENV variable: REACT_APP_HOST_IP: ' + process.env.REACT_APP_HOST_IP
+)
 
 export const API_HOST =
   process.env.NODE_ENV === 'production'
     ? process.env.REACT_APP_HOST_IP
       ? `http://${process.env.REACT_APP_HOST_IP}:${port}`
-      : process.env.REACT_APP_HOST
-      ? `http://${process.env.REACT_APP_HOST}:${port}`
       : '.'
     : `http://localhost:${port}`
-
-// ? process.env.REACT_APP_SERVER_IP
-//  ? `http://${process.env.REACT_APP_SERVER_IP}:${port}`
