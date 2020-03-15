101d737a551030106f9ec574a73b83ac7a844c78
D. Galakhov <dmitri.galakhov@gmail.com>
2020-03-15 04:04:25 +0100

    Fix outputs: escape env vars with an extra dollar sign

--- client/src/utils/constants/index.js
+++ client/src/utils/constants/index.js
@@ -2,9 +2,7 @@
 // For successful deployment, add your domain in here (also needed for CI/CD)
 const port = process.env.REACT_APP_HOST_PORT || 8080
 
-console.log(
-  'Passed ENV variable: REACT_APP_HOST: ' + process.env.REACT_APP_HOST
-)
+console.log('ENV variable REACT_APP_HOST: ' + process.env.REACT_APP_HOST)
 
 export const API_HOST =
   process.env.NODE_ENV === 'production'
